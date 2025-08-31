533617541fd2f0daa4b2059e552c5d264abaff3c
jerkey <jerkey@g-cipher.net>
2025-08-31 02:28:40 -0700

    add express slonik cors sps-common, enable dist/.vite/manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
-	server: {
-		port: 8995,
-		allowedHosts: ['securepollingsystem.org','securepollingsystem.com','stemgrid.org']
-	}
+  plugins: [preact()],
+  server: { // for vite dev server
+    port: 8995,
+    allowedHosts: ['securepollingsystem.org','securepollingsystem.com','stemgrid.org']
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    // see https://vite.dev/guide/backend-integration.html
+    manifest: true
+  },
 });
