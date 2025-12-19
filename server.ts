2fd885353143e1c1fd06de0a7ea8a6d9c0d87d1e
jake watters <user@raz.lap>
2025-12-19 01:02:17 -0800

    slop move config to server/config.ts

--- server.ts
+++ server.ts
@@ -4,49 +4,14 @@ import fs from 'fs';
 import cors from 'cors';
 import { verifyScreedSignature } from 'sps-common';
 import YAML from 'yaml';
-
-async function readConfig(configObj, varName, defaultVal) { // case-insensitive varName search
-  const objVarName = Object.keys(configObj).find(key => key.toLowerCase() === varName.toLowerCase());
-  const configVal = configObj[objVarName]; // undefined if doesn't exist
-  if (['number','string','object'].includes(typeof(configVal))) {
-    return configVal;
-  } else {
-    console.log('didnt find',varName,'using default',defaultVal);
-    return defaultVal;
-  };
-}
+import { loadConfig } from './server/config';
 
 var configFileName = process.env.SPS_CONFIG_FILE; // check environment for SPS_CONFIG_FILE
 if (configFileName === undefined) {
   configFileName = import.meta.url.replace('file://','') + '.yaml'; // default config filename is this file .yaml
 }
 
-var config;
-try {
-  config = await YAML.parse(fs.readFileSync(configFileName, {encoding: 'utf8'}));
-} catch (err) {
-  console.error('Error reading config file ' + configFileName, err);
-}
-var logFileName = await readConfig(config, 'logfile', 'sps-counter.log');
-var allowedOrigins = await readConfig(config, 'allowedOrigins', []); // URLs that are allowed to connect here
-//console.log('allowedOrigins',allowedOrigins);
-var blockList = await readConfig(config, 'blockList', []); // IP addresses or prefixes that we simply ignore
-//console.log('blockList',blockList);
-var serverPort = await readConfig(config, 'serverPort', 8994); // port on which this server listens (default 8994)
-var postGresURI = await readConfig(config, 'postGresURI', undefined); // postgresql://[user[:password]@][host[:port]][/database name][?name=value[&...]]
-if (postGresURI === undefined) {
-  const postgresconfig = await readConfig(config, 'postgres', undefined);
-  if (postgresconfig === undefined) {
-    console.log('postgres URI is not defined in configuration file')
-    process.exit(9);
-  }
-  postGresURI = 'postgresql://'
-        + await readConfig(postgresconfig, 'user', 'postgres') + ':'
-        + await readConfig(postgresconfig, 'password', 'postgres') + '@'
-        + await readConfig(postgresconfig, 'address', 'localhost') + ':'
-        + await readConfig(postgresconfig, 'port', 5432) + '/'
-        + await readConfig(postgresconfig, 'database', 'postgres');
-}
+var { config, logFileName, allowedOrigins, blockList, serverPort, postGresURI } = await loadConfig();
 
 // TODO: use postgres to store our uptime
 // TODO: use log4js instead of this
---
+++ server/config.ts
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import YAML from 'yaml';
+
+export async function readConfig(configObj: any, varName: string, defaultVal: any) {
+  const objVarName = Object.keys(configObj || {}).find(key => key.toLowerCase() === varName.toLowerCase());
+  const configVal = objVarName ? configObj[objVarName] : undefined;
+  if (['number','string','object'].includes(typeof(configVal))) {
+    return configVal;
+  } else {
+    console.log('didnt find',varName,'using default',defaultVal);
+    return defaultVal;
+  }
+}
+
+export async function loadConfig() {
+  let configFileName = process.env.SPS_CONFIG_FILE; // check environment for SPS_CONFIG_FILE
+  if (configFileName === undefined) {
+    configFileName = import.meta.url.replace('file://','') + '.yaml'; // default config filename is this file .yaml
+  }
+
+  let config: any = {};
+  try {
+    config = YAML.parse(fs.readFileSync(configFileName, {encoding: 'utf8'}));
+  } catch (err) {
+    console.error('Error reading config file ' + configFileName, err);
+  }
+
+  const logFileName = await readConfig(config, 'logfile', 'sps-counter.log');
+  const allowedOrigins = await readConfig(config, 'allowedOrigins', []); // URLs that are allowed to connect here
+  const blockList = await readConfig(config, 'blockList', []); // IP addresses or prefixes that we simply ignore
+  const serverPort = await readConfig(config, 'serverPort', 8994); // port on which this server listens (default 8994)
+  let postGresURI = await readConfig(config, 'postGresURI', undefined); // postgresql://[user[:password]@][host[:port]][/database name][?name=value[&...]]
+  if (postGresURI === undefined) {
+    const postgresconfig = await readConfig(config, 'postgres', undefined);
+    if (postgresconfig === undefined) {
+      console.log('postgres URI is not defined in configuration file')
+      process.exit(9);
+    }
+    postGresURI = 'postgresql://'
+          + await readConfig(postgresconfig, 'user', 'postgres') + ':'
+          + await readConfig(postgresconfig, 'password', 'postgres') + '@'
+          + await readConfig(postgresconfig, 'address', 'localhost') + ':'
+          + await readConfig(postgresconfig, 'port', 5432) + '/'
+          + await readConfig(postgresconfig, 'database', 'postgres');
+  }
+
+  return { config, logFileName, allowedOrigins, blockList, serverPort, postGresURI };
+}
